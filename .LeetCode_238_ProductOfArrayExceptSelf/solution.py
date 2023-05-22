31409a463de44a64b96f619bf2dd90cb6ea88f9c
Marcel Senftleben <marcel@kirsti.de>
2023-05-23 01:49:29 +0200

    238_1

---
+++ .LeetCode_238_ProductOfArrayExceptSelf/solution.py
@@ -0,0 +1,48 @@
+#https://leetcode.com/problems/product-of-array-except-self/
+import collections
+def productExceptSelf(nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        #calculate prefix list
+        prefix = []
+        for i in range(len(nums)):
+            if i <= 0:
+                 prefix.append(nums[i])
+                 continue
+            prefix.append(nums[i]*prefix[i-1])
+
+        #calculate postfix list
+        postfix = [0]*len(nums)
+        for i in range(len(nums) -1, -1, -1):
+            #print(i)
+            if i == len(nums)-1:
+                 postfix[i] = nums[i]
+                 continue
+            postfix[i] = nums[i]*postfix[i+1]
+        #calculate the output
+        output = []
+        for i in range(len(nums)):
+            if i == 0:
+                 output.append(postfix[i+1])
+            elif i == len(nums)-1:
+                 output.append(prefix[i-1])
+            else:
+                 output.append(prefix[i-1]*postfix[i+1])
+
+        print("prefix:", prefix)
+        print("postfix:", postfix)
+        return output
+
+
+
+
+
+def main():
+    print("Test1: nums = [-1,1,0,-3,3]")
+    res = productExceptSelf([-1,1,0,-3,3])
+    print("Result", res)
+
+if __name__ == "__main__":
+    main()
